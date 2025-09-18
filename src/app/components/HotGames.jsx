7ab0156e7e22327b7f1ebfd540864de69d9c4ae4
Ahiya9692 <ahiyafiverr@gmail.com>
2025-09-18 23:03:37 +0600

    Update HotGames.jsx

--- src/app/components/HotGames.jsx
+++ src/app/components/HotGames.jsx
@@ -26,7 +26,7 @@ const HotGames = () => {
                     <Link href={data.link} > <Image src={data.img} width={200} height={300} alt="Game" className="rounded-lg" /></Link>
                 </div>)} */}
                 <Link href="/" > <Image src="/poker.jpg" width={300} height={300} alt="Game" className="rounded-lg mx-auto" /></Link>
-                <Link href="https://game.pokerwin.cc/0301/web-mobile/?328e9b47e1d82f069b5d1dd7fbc0e18ffab763d89882a59dcb59455ee7370ee9&eyJTTiI6IjQ2NDk0N0M3ODc1NjQ4NDlCQzg0NDZEMzlEQjA3NDk2IiwiSUQiOiIxYzVlYmI5MmQzMjE0OGJmOTQ0NzJiZWRmNWQ4ODgxMiIsIk1ldGhvZCI6IkdldExvZ2luSDUiLCJMb2dpbklkIjoiSkJQTTAwMDAwMDAwMDAyNTY1MTYiLCJMYW5ndWFnZSI6IkVuX3VzIiwiU2lnbmF0dXJlIjoiMjNkNDhkODg4MDczYTMxNmYwZmI5OGIxNTkzYzhmZDcifQ==" className="w-[300px] mx-auto flex items-center justify-center text-center px-6 py-2 bg-amber-400 text-black">Play Now</Link>
+                <Link href="https://game.pokerwin.cc/0301/web-mobile/?2fa19d23d7b8d8e478e86a7b84bd492b41cf0ccaff6da41b796db1cb20b50086&eyJTTiI6IjQ2NDk0N0M3ODc1NjQ4NDlCQzg0NDZEMzlEQjA3NDk2IiwiSUQiOiI2MzM5NmJiNWU0YTE0YmY4YTFlZDhkYjRjMTFlNjIwZCIsIk1ldGhvZCI6IkdldExvZ2luSDUiLCJMb2dpbklkIjoiSkJQTTAwMDAwMDAwMDAyNTY1MTYiLCJMYW5ndWFnZSI6IkVuX3VzIiwiU2lnbmF0dXJlIjoiOWQwYzhkZTNjMzM0MDJhZWEwODdkNTBkZDgwNTkyOWIifQ==" className="w-[300px] mx-auto flex items-center justify-center text-center px-6 py-2 bg-amber-400 text-black">Play Now</Link>
             </div>
         </div>
 
@@ -34,6 +34,7 @@ const HotGames = () => {
 }
 
 export default HotGames 
+
 
 
 
